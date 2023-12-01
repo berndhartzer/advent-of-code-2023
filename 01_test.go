6df12f81e61682faf512b367f69393dab9e8ec3b
Bernd Hartzer <berndhartzer@gmail.com>
2023-12-01 16:30:19 +1000

    day 1 part 2

--- 01_test.go
+++ 01_test.go
@@ -2,6 +2,7 @@ package aoc
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 	"time"
 )
@@ -38,6 +39,109 @@ func dayOnePartOne(input []string) int {
 		// e.g. 49 - 48 == 1
 		total += (int(first)-48)*10
 		total += (int(last)-48)
+	}
+
+	return total
+}
+
+func dayOnePartTwo(input []string) int {
+	total := 0
+
+	for _, line := range input {
+		l := 0
+		r := len(line)-1
+
+		gotFirst, gotLast := false, false
+		first, last := 0, 0
+
+		for {
+			// 49 == 1, 57 == 9
+			if !gotFirst && line[l] >= 49 && line[l] <= 57 {
+				first = int(line[l])-48
+				gotFirst = true
+			}
+			if !gotLast && line[r] >= 49 && line[r] <= 57 {
+				last = int(line[r])-48
+				gotLast = true
+			}
+
+			if !gotFirst {
+				tmpStr := line[l:len(line)]
+
+				switch {
+				case strings.HasPrefix(tmpStr, "one"):
+					first = 1
+					gotFirst = true
+				case strings.HasPrefix(tmpStr, "two"):
+					first = 2
+					gotFirst = true
+				case strings.HasPrefix(tmpStr, "three"):
+					first = 3
+					gotFirst = true
+				case strings.HasPrefix(tmpStr, "four"):
+					first = 4
+					gotFirst = true
+				case strings.HasPrefix(tmpStr, "five"):
+					first = 5
+					gotFirst = true
+				case strings.HasPrefix(tmpStr, "six"):
+					first = 6
+					gotFirst = true
+				case strings.HasPrefix(tmpStr, "seven"):
+					first = 7
+					gotFirst = true
+				case strings.HasPrefix(tmpStr, "eight"):
+					first = 8
+					gotFirst = true
+				case strings.HasPrefix(tmpStr, "nine"):
+					first = 9
+					gotFirst = true
+				}
+			}
+			if !gotLast {
+				tmpStr := line[0:r+1]
+
+				switch {
+				case strings.HasSuffix(tmpStr, "one"):
+					last = 1
+					gotLast = true
+				case strings.HasSuffix(tmpStr, "two"):
+					last = 2
+					gotLast = true
+				case strings.HasSuffix(tmpStr, "three"):
+					last = 3
+					gotLast = true
+				case strings.HasSuffix(tmpStr, "four"):
+					last = 4
+					gotLast = true
+				case strings.HasSuffix(tmpStr, "five"):
+					last = 5
+					gotLast = true
+				case strings.HasSuffix(tmpStr, "six"):
+					last = 6
+					gotLast = true
+				case strings.HasSuffix(tmpStr, "seven"):
+					last = 7
+					gotLast = true
+				case strings.HasSuffix(tmpStr, "eight"):
+					last = 8
+					gotLast = true
+				case strings.HasSuffix(tmpStr, "nine"):
+					last = 9
+					gotLast = true
+				}
+			}
+
+			if gotFirst && gotLast {
+				break
+			}
+
+			l++
+			r--
+		}
+
+		total += first*10
+		total += last
 	}
 
 	return total
@@ -84,5 +188,16 @@ func TestDayOne(t *testing.T) {
 		}
 
 		runTests(t, tests, dayOnePartOne)
+	})
+
+	t.Run("part two", func(t *testing.T) {
+		tests := map[string]testConfig{
+			"solution": {
+				input:     input,
+				logResult: true,
+			},
+		}
+
+		runTests(t, tests, dayOnePartTwo)
 	})
 }
--- README.md
+++ README.md
@@ -10,3 +10,5 @@ go test -v -run=^TestDayOne
 go test -v -run=^TestDayOne/part_one
 go test -v -run=^TestDayOne/part_one/solution
 ```
+
+Input is downloaded automatically if not found. To auth with adventofcode.com we need a session cookie. Get this cookie and create a file at `input/.session`. The contents of the file should be the session cookie value. Note the `input/` directory is not tracked by git to stop leaking credentials and test inputs.
