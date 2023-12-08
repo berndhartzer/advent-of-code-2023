05c16f90839d2cba98dd64684bcd830064891403
Bernd Hartzer <berndhartzer@gmail.com>
2023-12-08 16:58:20 +1000

    day 8

---
+++ 08.go
@@ -0,0 +1,225 @@
+package aoc
+
+import (
+	"fmt"
+)
+
+type node struct {
+	id    string
+	left  *node
+	right *node
+}
+
+func buildNetwork(network []string) map[string]*node {
+	allNodes := map[string]*node{}
+
+	for _, line := range network {
+		parent := line[:3]
+		left := line[7:10]
+		right := line[12:15]
+
+		var leftNode, rightNode, parentNode *node
+
+		gotLeft, ok := allNodes[left]
+		if ok {
+			leftNode = gotLeft
+		} else {
+			leftNode = &node{
+				id: left,
+			}
+			allNodes[left] = leftNode
+		}
+
+		gotRight, ok := allNodes[right]
+		if ok {
+			rightNode = gotRight
+		} else {
+			rightNode = &node{
+				id: right,
+			}
+			allNodes[right] = rightNode
+		}
+
+		gotParent, ok := allNodes[parent]
+		if ok {
+			parentNode = gotParent
+			parentNode.left = leftNode
+			parentNode.right = rightNode
+		} else {
+			parentNode = &node{
+				id:    parent,
+				left:  leftNode,
+				right: rightNode,
+			}
+			allNodes[parent] = parentNode
+		}
+	}
+
+	return allNodes
+}
+
+func dayEightPartOne(input []string) int {
+	instructions := input[0]
+	network := input[2:]
+
+	allNodes := buildNetwork(network)
+
+	curr := allNodes["AAA"]
+	steps := 0
+
+	looper := 0
+	for {
+		steps++
+
+		switch instructions[looper] {
+		case 'L':
+			curr = curr.left
+		case 'R':
+			curr = curr.right
+		}
+
+		if curr.id == "ZZZ" {
+			break
+		}
+
+		looper++
+		if looper > len(instructions)-1 {
+			looper = 0
+		}
+	}
+
+	return steps
+}
+
+func dayEightPartTwo(input []string) int {
+	instructions := input[0]
+	network := input[2:]
+
+	allNodes := buildNetwork(network)
+
+	ghosts := []*node{}
+	ghostSteps := []int{}
+	for k, v := range allNodes {
+		if k[2] == 'A' {
+			ghosts = append(ghosts, v)
+			ghostSteps = append(ghostSteps, 0)
+		}
+	}
+
+	looper := 0
+	for {
+		activeGhosts := false
+
+		for i := 0; i < len(ghosts); i++ {
+			if ghosts[i] == nil {
+				continue
+			}
+			activeGhosts = true
+
+			ghostSteps[i]++
+
+			switch instructions[looper] {
+			case 'L':
+				ghosts[i] = ghosts[i].left
+			case 'R':
+				ghosts[i] = ghosts[i].right
+			}
+
+			if ghosts[i].id[2] != 'Z' {
+				continue
+			}
+
+			ghosts[i] = nil
+		}
+
+		if !activeGhosts {
+			break
+		}
+
+		looper++
+		if looper > len(instructions)-1 {
+			looper = 0
+		}
+	}
+
+	return LCM(ghostSteps[0], ghostSteps[1], ghostSteps[2:]...)
+}
+
+type dayEightTestConfig struct {
+	input     []string
+	expected  int
+	logResult bool
+}
+
+func getDayEightTests() (map[string]dayEightTestConfig, map[string]dayEightTestConfig, error) {
+	fileInput, err := getInput(8)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to get input: %v", err)
+	}
+	input := fileInput.asStringSlice()
+
+	partOne := map[string]dayEightTestConfig{
+		"1": {
+			input: []string{
+				"LLR",
+				"",
+				"AAA = (BBB, BBB)",
+				"BBB = (AAA, ZZZ)",
+				"ZZZ = (ZZZ, ZZZ)",
+			},
+			expected: 6,
+		},
+		"solution": {
+			input:     input,
+			logResult: true,
+		},
+	}
+
+	partTwo := map[string]dayEightTestConfig{
+		"1": {
+			input: []string{
+				"LR",
+				"",
+				"11A = (11B, XXX)",
+				"11B = (XXX, 11Z)",
+				"11Z = (11B, XXX)",
+				"22A = (22B, XXX)",
+				"22B = (22C, 22C)",
+				"22C = (22Z, 22Z)",
+				"22Z = (22B, 22B)",
+				"XXX = (XXX, XXX)",
+			},
+			expected: 6,
+		},
+		"solution": {
+			input:     input,
+			logResult: true,
+		},
+	}
+
+	return partOne, partTwo, nil
+}
+
+// Credit due:
+// https://go.dev/play/p/SmzvkDjYlb
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
+}
--- solutions_test.go
+++ solutions_test.go
@@ -176,4 +176,38 @@ func TestSolutions(t *testing.T) {
 			runner(t, partTwo, daySixPartTwo)
 		})
 	})
+
+	t.Run("day 8", func(t *testing.T) {
+		runner := func(t *testing.T, tests map[string]dayEightTestConfig, fn func([]string) int) {
+			for name, cfg := range tests {
+				cfg := cfg
+				t.Run(name, func(t *testing.T) {
+					start := time.Now()
+					output := fn(cfg.input)
+					finish := time.Since(start)
+					if cfg.logResult {
+						t.Log(fmt.Sprintf("\nsolution:\t%v\nelapsed time:\t%s", output, finish))
+						return
+					}
+
+					if output != cfg.expected {
+						t.Fatalf("Incorrect output - got: %v, want: %v", output, cfg.expected)
+					}
+				})
+			}
+		}
+
+		partOne, partTwo, err := getDayEightTests()
+		if err != nil {
+			t.Errorf("failed to get tests: %v", err)
+		}
+
+		t.Run("part 1", func(t *testing.T) {
+			runner(t, partOne, dayEightPartOne)
+		})
+
+		t.Run("part 2", func(t *testing.T) {
+			runner(t, partTwo, dayEightPartTwo)
+		})
+	})
 }
