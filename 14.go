07989a86e4d25364aeecf697ec01c3d562cdd83f
Bernd Hartzer <berndhartzer@gmail.com>
2023-12-15 08:13:55 +1000

    day 14 pt 2

--- 14.go
+++ 14.go
@@ -1,6 +1,7 @@
 package aoc
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
@@ -11,7 +12,6 @@ func dayFourteenPartOne(input []string) int {
 
 	col, row, empty := 0, 0, 0
 	for {
-
 		switch input[row][col] {
 		case 'O':
 			load := topLoad - (row - empty)
@@ -39,7 +39,181 @@ func dayFourteenPartOne(input []string) int {
 }
 
 func dayFourteenPartTwo(input []string) int {
-	return 0
+	grid := make([][]byte, len(input))
+
+	for row, line := range input {
+		gridRow := make([]byte, len(line))
+		for i, c := range line {
+			switch c {
+			case 'O', '#':
+				gridRow[i] = byte(c)
+			}
+		}
+		grid[row] = gridRow
+	}
+
+	tiltNorth := func() {
+		col, row, empty := 0, 0, 0
+		for {
+			switch grid[row][col] {
+			case 'O':
+				if empty > 0 {
+					grid[row-empty][col] = 'O'
+					grid[row][col] = byte(0)
+				}
+			case '#':
+				empty = 0
+			default:
+				empty++
+			}
+
+			row++
+
+			if row >= len(grid) {
+				row = 0
+				empty = 0
+				col++
+			}
+
+			if col >= len(grid[0]) {
+				break
+			}
+		}
+	}
+
+	tiltWest := func() {
+		for row := 0; row < len(grid); row++ {
+			empty := 0
+			for col := 0; col < len(grid[0]); col++ {
+				switch grid[row][col] {
+				case 'O':
+					if empty > 0 {
+						grid[row][col-empty] = 'O'
+						grid[row][col] = byte(0)
+					}
+				case '#':
+					empty = 0
+				default:
+					empty++
+				}
+			}
+		}
+	}
+
+	tiltSouth := func() {
+		col, row, empty := 0, len(grid)-1, 0
+		for {
+			switch grid[row][col] {
+			case 'O':
+				if empty > 0 {
+					grid[row+empty][col] = 'O'
+					grid[row][col] = byte(0)
+				}
+			case '#':
+				empty = 0
+			default:
+				empty++
+			}
+
+			row--
+
+			if row < 0 {
+				row = len(grid) - 1
+				empty = 0
+				col++
+			}
+
+			if col >= len(grid[0]) {
+				break
+			}
+		}
+	}
+
+	tiltEast := func() {
+		for row := 0; row < len(grid); row++ {
+			empty := 0
+			for col := len(grid[0]) - 1; col >= 0; col-- {
+				switch grid[row][col] {
+				case 'O':
+					if empty > 0 {
+						grid[row][col+empty] = 'O'
+						grid[row][col] = byte(0)
+					}
+				case '#':
+					empty = 0
+				default:
+					empty++
+				}
+			}
+		}
+	}
+
+	cycle := func() {
+		tiltNorth()
+		tiltWest()
+		tiltSouth()
+		tiltEast()
+	}
+
+	cache := map[string]int{}
+	breakpoint := -1
+
+	for i := 0; i < 1_000_000_000; i++ {
+		cycle()
+
+		state, err := json.Marshal(grid)
+		if err != nil {
+			panic("failed to marshal grid")
+		}
+
+		if breakpoint == -1 {
+			v, ok := cache[string(state)]
+			if ok {
+				breakpoint = i + (1_000_000_000-v)%(i-v) - 1
+				continue
+			}
+
+			cache[string(state)] = i
+		}
+
+		if i != breakpoint {
+			continue
+		}
+
+		break
+	}
+
+	// Score the grid
+	total := 0
+
+	topLoad := len(grid)
+
+	col, row, empty := 0, 0, 0
+	for {
+		switch grid[row][col] {
+		case 'O':
+			load := topLoad - row
+			total += load
+		case '#':
+			empty = 0
+		default:
+			empty++
+		}
+
+		row++
+
+		if row >= len(grid) {
+			row = 0
+			empty = 0
+			col++
+		}
+
+		if col >= len(grid[0]) {
+			break
+		}
+	}
+
+	return total
 }
 
 func dayFourteenTests() (map[string]stringSliceToIntTestConfig, map[string]stringSliceToIntTestConfig, error) {
@@ -72,6 +246,21 @@ func dayFourteenTests() (map[string]stringSliceToIntTestConfig, map[string]strin
 	}
 
 	partTwo := map[string]stringSliceToIntTestConfig{
+		"1": {
+			input: []string{
+				"O....#....",
+				"O.OO#....#",
+				".....##...",
+				"OO.#O....O",
+				".O.....O#.",
+				"O.#..O.#.#",
+				"..O..#O..O",
+				".......O..",
+				"#....###..",
+				"#OO..#....",
+			},
+			expected: 64,
+		},
 		"solution": {
 			input:     input,
 			logResult: true,
